4158d2730374019a1a6f82d218ad32faf1f71eb5
Vontainment <djav1985@gmail.com>
2025-07-09 10:55:38 -0400

    Add 404 view and ensure routing

---
+++ root/app/Views/404.php
@@ -0,0 +1,24 @@
+<?php
+// phpcs:ignoreFile PSR1.Files.SideEffects.FoundWithSymbols
+
+/**
+ * Project: SocialRSS
+ * Author:  Vontainment <services@vontainment.com>
+ * License: https://opensource.org/licenses/MIT MIT License
+ * Link:    https://vontainment.com
+ * Version: 3.0.0
+ *
+ * File: 404.php
+ * Description: Page not found view
+ */
+
+require 'layouts/header.php';
+?>
+
+<main class="container">
+    <div class="empty">
+        <p class="empty-title">Page not found</p>
+    </div>
+</main>
+
+<?php require 'layouts/footer.php'; ?>
